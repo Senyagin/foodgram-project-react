0a6347314fec6dbf0c0be71629ffb9b7968c4191
Senyagin <110063557+Senyagin@users.noreply.github.com>
2023-08-14 10:42:36 +0300

    Delete backend/foodgram directory

--- backend/foodgram/api/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiConfig(AppConfig):
-    """Конфигурация для приложения API."""
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
--- backend/foodgram/users/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersConfig(AppConfig):
-    """Конфигурация для приложения User."""
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
