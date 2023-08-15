2f5c26cf89343db44afa453799e393f9c0bd7a23
Сергей Сенягин <senyagin.serg@yandex.ru>
2023-08-15 22:08:57 +0300

    finish

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -35,25 +35,38 @@ class IngredientAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 
+class IngredientsAmountInline(admin.TabularInline):
+    """
+    Позволяет выводить кол-во ингредиентов в карточке рецепта через модель
+    IngredientsAmount.
+    """
+    model = AmountIngredients
+    extra = 1
+
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-        'author',
-        'count_favorites',
-    )
-    list_filter = (
-        'author',
-        'name',
-        'tags'
-    )
-    search_fields = (
-        'name',
-        'author__username'
-    )
+    list_display = ('name', 'author', 'display_tags')
+    list_filter = ('name', 'author', 'tags')
+    search_fields = ('name', 'author__username', 'author__last_name',
+                     'author__first_name', 'tags__name')
+    readonly_fields = ('favorite_count', 'shopping_count')
+    filter_vertical = ('tags', 'ingredients')
+    inlines = (IngredientsAmountInline,)
+    empty_value_display = '--пусто--'
 
-    def count_favorites(self, obj: Recipe):
-        return obj.favorite.count()
+    def favorite_count(self, obj):
+        return obj.favorites.count()
+
+    def shopping_count(self, obj):
+        return obj.shopcarts.count()
+
+    def display_tags(self, obj):
+        return ', '.join([tag.name for tag in obj.tags.all()])
+
+    favorite_count.short_description = 'В избранном'
+    shopping_count.short_description = 'В списке покупок'
+    display_tags.short_description = 'Теги'
 
 
 @admin.register(AmountIngredients)
